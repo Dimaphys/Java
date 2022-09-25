5df4492a00e7f99ca4e9b95e41d078d6a1093905
Dmitrii Vasilev <dimaphys@gmail.com>
2022-09-25 20:42:51 +0300

    Homework1

---
+++ src/main/java/homework1/Task1.java
@@ -0,0 +1,27 @@
+package homework1;
+
+public class Task1 {
+    public static void main(String[] args){
+        char one = 'G';
+        System.out.println("char: "+one);
+
+        int two = 89;
+        System.out.println("integer: "+two);
+
+        byte three = 4;
+        System.out.println("byte: "+three);
+
+        short four = 56;
+        System.out.println("short: "+four);
+
+        float five = 4.7333436f;
+        System.out.println("float: "+five);
+
+        double six = 4.355453532;
+        System.out.println("double: "+six);
+
+        long seven = 12121;
+        System.out.println("long: "+seven);
+
+    }
+}
---
+++ src/main/java/homework1/Task2.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 689;
+        System.out.println(a/100);
+        System.out.println(a%100/10);
+        System.out.println(a%10);
+    }
+    }
+
