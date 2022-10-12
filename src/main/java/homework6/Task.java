2e8e07032362e10f3e08d2820a244e406e3b3893
Dmitrii Vasilev <dimaphys@gmail.com>
2022-10-12 14:42:25 +0300

    homework6

--- src/main/java/homework6/Task.java
+++ src/main/java/homework6/Task.java
@@ -8,32 +8,38 @@ public class Task {
 
         System.out.println("суммарное количество секунд до конца дня, суммарное количество минут до конца дня и суммарное количество часов до конца рабочего дня \nВведите какой сейчас час");
         int hrs = scanner.nextInt();
-        if (hrs <= 17 && hrs >= 8){
+        if (hrs <= 17 && hrs >= 8) {
             System.out.println("Введите сколько сейчас минут");
             int min = scanner.nextInt();
-            if (min <= 60 && min >= 0){
-               int hoursleft = hours(hrs, min);
-               int minleft = minutes(min, hoursleft);
-               int secleft = sec(minleft);
-               System.out.println("Количество оставшихся рабочих часов: " + hoursleft);
-               System.out.println("Количество оставшихся рабочих минут: " + minleft);
-               System.out.println("Количество оставшихся рабочих секунд: " + secleft);
-           }
+            if (min <= 60 && min >= 0) {
+                int hoursleft = hours(hrs, min);
+                int minleft = minutes(min, hoursleft);
+                int secleft = sec(minleft);
+                System.out.println("Количество оставшихся рабочих часов: " + hoursleft);
+                System.out.println("Количество оставшихся рабочих минут: " + minleft);
+                System.out.println("Количество оставшихся рабочих секунд: " + secleft);
+            } else {
+                System.out.println("Неверно введено значение");
+            }
+        } else {
+            System.out.println("Неверно введено значение");
         }
-
+        scanner.close();
     }
 
-    private static int hours(int hrs, int min){
-        int a = min > 0 ? 16:17;
-        return a-hrs;
+
+    private static int hours(int hrs, int min) {
+        int a = min > 0 ? 16 : 17;
+        return a - hrs;
     }
-    private static int minutes(int a, int b){
-        return (60-a)+b*60;
+
+    private static int minutes(int a, int b) {
+        return (60 - a) + b * 60;
     }
-    private static int sec(int a){
+
+    private static int sec(int a) {
         return a * 60;
     }
-
 
 
 }
